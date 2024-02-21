5c98d3a4b09be7a93e8de5aa5044a5437eec6f85
kunal deepak patil <132162026+kunalpro379@users.noreply.github.com>
2024-02-21 09:22:19 +0530

    Update

---
+++ djktras.py
@@ -0,0 +1,38 @@
+import heapq
+
+class Dijkstra:
+    def __init__(self, graph):
+        self.graph = graph
+
+    def find_shortest_paths(self, start):
+        distances = {vertex: float('infinity') for vertex in self.graph}
+        distances[start] = 0
+        priority_queue = [(0, start)]
+
+        while priority_queue:
+            current_distance, current_vertex = heapq.heappop(priority_queue)
+
+            if current_distance > distances[current_vertex]:
+                continue
+
+            for neighbor, weight in self.graph[current_vertex].items():
+                distance = current_distance + weight
+
+                if distance < distances[neighbor]:
+                    distances[neighbor] = distance
+                    heapq.heappush(priority_queue, (distance, neighbor))
+
+        return distances
+
+# Example usage:
+graph = {
+    'A': {'B': 1, 'C': 4},
+    'B': {'A': 1, 'C': 2, 'D': 5},
+    'C': {'A': 4, 'B': 2, 'D': 1},
+    'D': {'B': 5, 'C': 1}
+}
+
+start_vertex = 'A'
+dijkstra_instance = Dijkstra(graph)
+result = dijkstra_instance.find_shortest_paths(start_vertex)
+print(result)
